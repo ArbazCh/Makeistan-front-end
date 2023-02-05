291f958d9434fd1283c861a75a76b55facadab0d
ArbazCh <72260630+ArbazCh@users.noreply.github.com>
2023-02-05 17:46:31 +0500

    FINAL: todo- shipping calculation, checkout UI

--- my-app/src/api/axios.config.js
+++ my-app/src/api/axios.config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjMsImVtYWlsIjoiYXJiYXpjaDEzMTNAZ21haWwuY29tIiwiaWF0IjoxNjc0OTE3MjczfQ.-_NHk39rqYDgsgQjPCo8VwRtaMpDJWJjeqGVln5TPok";
 
-// const BASE_URL = "https://dummyjson.com";
+// const BASE_URL = "http://localhost:5000/api/customer";
 const BASE_URL = "http://localhost:5000/api/customer";
 
 const API = axios.create({
